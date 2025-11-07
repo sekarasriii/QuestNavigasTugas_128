35a8ed43e4c93cd17265bc788c4bdb60f8a65948
sekarasriii <sekareeesh@gmail.com>
2025-11-07 14:23:35 +0700

    menambahkan inisialisasi value untuk jenis kelamin

--- app/src/main/java/com/example/questnavigastugas_128/view/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/questnavigastugas_128/view/FormulirPendaftaran.kt
@@ -2,10 +2,17 @@ package com.example.questnavigastugas_128.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.material3.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormulirPendaftaran(
     viewModel: PesertaViewModel,
     onSubmitBtnClick: () -> Unit
-)
+){
+    val uiState by viewModel.uiState.collectAsState()
+    val jenisKelamin = listOf(
+        stringResource(id = R.string.Laki_laki),
+        stringResource(id = R.string.Perempuan)
+    )
