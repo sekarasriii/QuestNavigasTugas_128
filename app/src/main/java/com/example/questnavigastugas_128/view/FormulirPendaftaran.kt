3fd6cb445804e684872832cc0e36f6233eb34b7a
sekarasriii <sekareeesh@gmail.com>
2025-11-07 14:20:39 +0700

    menambahkan inisialisasi OptIn dan function Formulir Pendaftaran

---
+++ app/src/main/java/com/example/questnavigastugas_128/view/FormulirPendaftaran.kt
@@ -0,0 +1,11 @@
+package com.example.questnavigastugas_128.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormulirPendaftaran(
+    viewModel: PesertaViewModel,
+    onSubmitBtnClick: () -> Unit
+)
