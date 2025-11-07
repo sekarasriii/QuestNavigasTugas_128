43d93c52c05bea6a2d759760071ef9b4680119ce
sekarasriii <sekareeesh@gmail.com>
2025-11-07 15:51:21 +0700

    menambah state untuk form input dan list semua peserta yg sudah terdaftar

--- app/src/main/java/com/example/questnavigastugas_128/viewmodel/PesertaViewModel.kt
+++ app/src/main/java/com/example/questnavigastugas_128/viewmodel/PesertaViewModel.kt
@@ -1,8 +1,21 @@
 package com.example.questnavigastugas_128.viewmodel
 
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
 data class Peserta(
     val namaLengkap: String = "",
     val jenisKelamin: String = "",
     val statusPerkawinan: String = "",
     val alamat: String = ""
 )
+class PesertaViewModel : ViewModel() {
+    // State untuk form input saat ini
+    private val _uiState = MutableStateFlow(Peserta())
+    val uiState: StateFlow<Peserta> = _uiState.asStateFlow()
+
+    // State untuk list semua peserta yang sudah terdaftar
+    private val _listPeserta = MutableStateFlow<List<Peserta>>(emptyList())
+    val listPeserta: StateFlow<List<Peserta>> = _listPeserta.asStateFlow()
