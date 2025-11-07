5e1814a74437e5eeedccef9d75e1d511c566387d
sekarasriii <sekareeesh@gmail.com>
2025-11-07 13:51:41 +0700

    menambahkan judul selamat datang

--- app/src/main/java/com/example/questnavigastugas_128/view/HalamanSelamatDatang.kt
+++ app/src/main/java/com/example/questnavigastugas_128/view/HalamanSelamatDatang.kt
@@ -9,6 +9,11 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.foundation.layout.R
 import androidx.compose.foundation.layout.Arrangement
 import  androidx.compose.ui.Alignment
+import androidx.compose.material3.Text
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.colorResource
 
 
 @Composable
@@ -21,5 +26,12 @@ fun HalamanSelamatDatang(
             .padding(dimensionResource(id = R.dimen.padding_medium)),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    )
+    ){
+        // Judul Selamat Datang
+        Text(
+            text = stringResource(id = R.string.selamat_datang),
+            fontSize = dimensionResource(id = R.dimen.text_size_big_header).value.sp,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(id = R.color.purple_500)
+        )
 }
