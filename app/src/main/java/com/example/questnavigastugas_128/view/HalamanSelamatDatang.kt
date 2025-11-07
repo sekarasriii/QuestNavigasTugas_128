da54c36f2e05a0421e83aa9a1614a2117053ce97
sekarasriii <sekareeesh@gmail.com>
2025-11-07 13:49:36 +0700

    menambahkan column utama untuk layout halaman selamat datang

--- app/src/main/java/com/example/questnavigastugas_128/view/HalamanSelamatDatang.kt
+++ app/src/main/java/com/example/questnavigastugas_128/view/HalamanSelamatDatang.kt
@@ -1,8 +1,25 @@
 package com.example.questnavigastugas_128.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.foundation.layout.R
+import androidx.compose.foundation.layout.Arrangement
+import  androidx.compose.ui.Alignment
+
 
 @Composable
 fun HalamanSelamatDatang(
     onMulaiClick: () -> Unit
-){}
+){
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+}
