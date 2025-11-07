d8011003020672fcad8d989ab03e73c4eb35b1d5
sekarasriii <sekareeesh@gmail.com>
2025-11-07 15:15:41 +0700

    insialisasi optin dan function utama list peserta

---
+++ app/src/main/java/com/example/questnavigastugas_128/view/ListDaftarPeserta.kt
@@ -0,0 +1,12 @@
+package com.example.questnavigastugas_128.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListDaftarPeserta(
+    viewModel: PesertaViewModel,
+    onBerandaBtnClick: () -> Unit,
+    onTambahBtnClick: () -> Unit
+) {}
