3cb92189593944dd2ca26ad42e965471c7201700
sekarasriii <sekareeesh@gmail.com>
2025-11-07 15:59:35 +0700

    inisialisasi function DataApp

--- app/src/main/java/com/example/questnavigastugas_128/Navigasi.kt
+++ app/src/main/java/com/example/questnavigastugas_128/Navigasi.kt
@@ -1,7 +1,25 @@
 package com.example.questnavigastugas_128
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigastugas_128.viewmodel.PesertaViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.collectAsState
+
 enum class Navigasi {
     Home,
     Formulir,
     ListPeserta
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    viewModel: PesertaViewModel = viewModel()
+){
+    var showDialog by remember { mutableStateOf(false) }
+    val uiState by viewModel.uiState.collectAsState()
 }
