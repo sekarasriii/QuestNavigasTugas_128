ac59db3ea339ce54091ebd5caa8a8327d40ffd18
sekarasriii <sekareeesh@gmail.com>
2025-11-07 14:28:52 +0700

    menambahkan setup scaffold untuk tampilan layar

--- app/src/main/java/com/example/questnavigastugas_128/view/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/questnavigastugas_128/view/FormulirPendaftaran.kt
@@ -4,6 +4,12 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.material3.R
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.ui.res.colorResource
+import androidx.compose.material3.TopAppBarDefaults
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -20,4 +26,21 @@ fun FormulirPendaftaran(
         stringResource(id = R.string.janda),
         stringResource(id = R.string.lajang),
         stringResource(id = R.string.duda)
+    )
+
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.formulir_pendaftaran),
+                        color = colorResource(id = R.color.white)
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(id = R.color.purple_500)
+                )
+            )
+        }
     )
