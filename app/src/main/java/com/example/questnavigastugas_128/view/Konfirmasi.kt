8713e02ce1873694f510450a2a52d91efaf0b410
sekarasriii <sekareeesh@gmail.com>
2025-11-07 14:47:48 +0700

    inisialisasi function Konfirmasi data

---
+++ app/src/main/java/com/example/questnavigastugas_128/view/Konfirmasi.kt
@@ -0,0 +1,10 @@
+package com.example.questnavigastugas_128.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Konfirmasi(
+    peserta: Peserta,
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit
+){}
